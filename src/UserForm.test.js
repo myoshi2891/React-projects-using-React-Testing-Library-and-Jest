74c03591045b65e625351c1b5d821fe11c49a081
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-14 20:13:42 +0900

    Recording Function Calls

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -1,26 +1,41 @@
-import { render, screen } from '@testing-library/react'
-import user from '@testing-library/user-event'
-import UserForm from './UserForm'
+import { expect } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import UserForm from "./UserForm";
 
-test('it shows two inputs and button', () => { 
-    // render the component
-    render(<UserForm />)
-    // Manipulate the component or find an element in it
-    const inputs = screen.getAllByRole('textbox');
-    const button = screen.getByRole('button');
-    // Assertion - make sure the component is doing what we expect it to do
-    expect(inputs).toHaveLength(2);
-    expect(button).toBeInTheDocument();
-})
+test("it shows two inputs and button", () => {
+	// render the component
+	render(<UserForm onUserAdd={() => {}} />);
+	// Manipulate the component or find an element in it
+	const inputs = screen.getAllByRole("textbox");
+	const button = screen.getByRole("button");
+	// Assertion - make sure the component is doing what we expect it to do
+	expect(inputs).toHaveLength(2);
+	expect(button).toBeInTheDocument();
+});
 
-test("it calls onUserAdd when the form is submitted", () => {
-	render(<UserForm />);
+test("it calls onUserAdd when the form is submitted", async () => {
+	const argList = [];
+	const callback = (...args) => {
+		argList.push(args);
+	};
+
+	render(<UserForm onUserAdd={callback} />);
 
 	const [nameInput, emailInput] = screen.getAllByRole("textbox");
 
-	user.click(nameInput);
-	user.keyboard("jane");
+	await user.click(nameInput);
+	await user.keyboard("jane");
 
-	user.click(emailInput);
-	user.keyboard("jane@example.com");
+	await user.click(emailInput);
+	await user.keyboard("jane@example.com");
+
+	const button = screen.getByRole("button");
+
+	await user.click(button);
+
+	console.log(argList);
+
+	expect(argList).toHaveLength(1);
+	expect(argList[0][0]).toEqual({ name: "jane", email: "jane@example.com" });
 });
