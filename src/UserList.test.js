3e667e72cff977ce5217511c98676fd4a302ceb2
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-14 21:10:08 +0900

    Avoiding BeforeEach

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,29 +1,28 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row per user", () => {
+function renderComponent() {
 	const users = [
 		{ name: "John Doe", email: "john.doe@example.com" },
 		{ name: "Jane Doe", email: "jane.doe@example.com" },
 	];
-	// const { container } =
 	render(<UserList users={users} />);
 
-		// eslint-disable-next-line
-		// const rows = container.querySelectorAll("tbody tr");
+	return { users };
+}
 
-		// screen.logTestingPlaygroundURL();
-		const rows = within(screen.getByTestId("users")).getAllByRole("row");
+test("render one row per user", () => {
+	renderComponent();
+	// eslint-disable-next-line
+	// const rows = container.querySelectorAll("tbody tr");
+
+	// screen.logTestingPlaygroundURL();
+	const rows = within(screen.getByTestId("users")).getAllByRole("row");
 	expect(rows).toHaveLength(2);
 });
 
 test("render the email and name of each user", () => {
-	const users = [
-		{ name: "John Doe", email: "john.doe@example.com" },
-		{ name: "Jane Doe", email: "jane.doe@example.com" },
-	];
-	render(<UserList users={users} />);
-
+	const { users } = renderComponent();
 	// screen.logTestingPlaygroundURL();
 	for (let user of users) {
 		const name = screen.getByRole("cell", { name: user.name });
