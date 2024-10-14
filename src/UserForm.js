8d106f42b4a5e398a4399053511af9745ddafadd
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-14 20:31:12 +0900

    Querying Elements by Labels

--- src/UserForm.js
+++ src/UserForm.js
@@ -11,17 +11,25 @@ function UserForm({ onUserAdd }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name</label>
-        <input value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div>
-        <label>Email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <button>Add User</button>
-    </form>
+		<form onSubmit={handleSubmit}>
+			<div>
+				<label htmlFor="name">Name</label>
+				<input
+					id="name"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+			</div>
+			<div>
+				<label htmlFor="email">Enter Email</label>
+				<input
+					id="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+			</div>
+			<button>Add User</button>
+		</form>
   );
 }
 
