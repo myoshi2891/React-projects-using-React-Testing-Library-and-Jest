f71a41a353b3047b1aaf122c6349d53dd69db317
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-15 23:03:54 +0900

    When to Use Each Suffix

--- src/DataForm.test.js
+++ src/DataForm.test.js
@@ -4,7 +4,16 @@ import DataForm from "./DataForm";
 test("selecting different elements", () => {
 	render(<DataForm />);
 
-	const elements = [];
+	const elements = [
+		screen.getByRole("button"),
+		screen.getByLabelText("Email"),
+		screen.getByPlaceholderText("Red"),
+		screen.getByText("Enter Data"),
+		screen.getByDisplayValue("asdf@asdf.com"),
+		screen.getByAltText("data"),
+		screen.getByTitle(/ready to submit/i),
+		screen.getByTestId("image wrapper"),
+	];
 
 	for (let element of elements) {
 		expect(element).toBeInTheDocument();
