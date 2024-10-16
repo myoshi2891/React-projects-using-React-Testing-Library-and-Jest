c5edc3f6417ab94d29b177bb600cb8ede35c923f
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-16 22:35:22 +0900

    Looping Over Assertions

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -2,7 +2,7 @@ import { expect } from "@jest/globals";
 import { screen, render } from "@testing-library/react";
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("displays the primary language of the repository", () => {
+test("displays information about the repository", () => {
 	const repository = {
 		language: "JavaScript",
 		stargazers_count: 5,
@@ -12,7 +12,10 @@ test("displays the primary language of the repository", () => {
 
 	render(<RepositoriesSummary repository={repository} />);
 
-	const language = screen.getByText("JavaScript");
+	for (let key in repository) {
+		const value = repository[key];
+		const element = screen.getByText(value);
 
-	expect(language).toBeInTheDocument();
+		expect(element).toBeInTheDocument();
+	}
 });
