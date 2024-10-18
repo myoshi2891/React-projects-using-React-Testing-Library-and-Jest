3960a66498b6c731dfe67e1af6543cf86e949b75
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-18 22:05:21 +0900

    Solving the Act Warning

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -5,19 +5,29 @@ import RepositoriesListItem from "./RepositoriesListItem";
 function renderComponent() {
 	const repository = {
 		full_name: "facebook/react",
-		language: "JavaScript",
+		language: "Javascript",
 		description: "A js library",
 		owner: "facebook",
 		name: "react",
 		html_url: "https://github.com/facebook/react",
 	};
-  render(
+	render(
 		<MemoryRouter>
 			<RepositoriesListItem repository={repository} />
 		</MemoryRouter>
-  );
+	);
 }
 
-test("shows a link to the github homepage for this repository", () => {
+test("shows a link to the github homepage for this repository", async () => {
 	renderComponent();
+
+	await screen.findByRole("img", { name: "Javascript" });
 });
+
+const pause = () => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve();
+		}, 100);
+	});
+};
