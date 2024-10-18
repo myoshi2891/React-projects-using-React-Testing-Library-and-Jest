ff4ba63bab22dba997524bef3dfb0b75ade4d386
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-18 22:11:55 +0900

    Module Mocks

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import RepositoriesListItem from "./RepositoriesListItem";
 
+jest.mock("../tree/FileIcon", () => {
+	return () => {
+		return "File Icon Component";
+	};
+});
+
 function renderComponent() {
 	const repository = {
 		full_name: "facebook/react",
@@ -21,7 +27,7 @@ function renderComponent() {
 test("shows a link to the github homepage for this repository", async () => {
 	renderComponent();
 
-	await screen.findByRole("img", { name: "Javascript" });
+	// await screen.findByRole("img", { name: "Javascript" });
 });
 
 const pause = () => {
