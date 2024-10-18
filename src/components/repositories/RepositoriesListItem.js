7364b9b38a9b2472396bb3182c97672f5e71fceb
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-18 22:34:23 +0900

    Implementing the Feature

--- src/components/repositories/RepositoriesListItem.js
+++ src/components/repositories/RepositoriesListItem.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { MarkGithubIcon } from "@primer/octicons-react";
 import FileIcon from '../tree/FileIcon';
 import RepositoriesSummary from './RepositoriesSummary';
 
@@ -6,16 +7,21 @@ function RepositoriesListItem({ repository }) {
   const { full_name, language, description, owner, name } = repository;
 
   return (
-    <div className="py-3 border-b flex">
-      <FileIcon name={language} className="shrink w-6 pt-1" />
-      <div>
-        <Link to={`/repositories/${full_name}`} className="text-xl">
-          {owner.login}/<span className="font-bold">{name}</span>
-        </Link>
-        <p className="text-gray-500 italic py-1">{description}</p>
-        <RepositoriesSummary repository={repository} />
-      </div>
-    </div>
+		<div className="py-3 border-b flex">
+			<FileIcon name={language} className="shrink w-6 pt-1" />
+			<div>
+				<Link to={`/repositories/${full_name}`} className="text-xl">
+					{owner.login}/<span className="font-bold">{name}</span>
+				</Link>
+				<p className="text-gray-500 italic py-1">{description}</p>
+				<RepositoriesSummary repository={repository} />
+			</div>
+			<div className="grow flex items-center justify-end pr-2">
+				<a href={repository.html_url} aria-label="github repository">
+					<MarkGithubIcon />
+				</a>
+			</div>
+		</div>
   );
 }
 
