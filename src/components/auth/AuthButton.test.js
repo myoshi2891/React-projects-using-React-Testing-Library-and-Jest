1c34fb57f79df4a963fa1baf666c5338f304735f
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-19 19:59:12 +0900

    Scoping Test Hooks

--- src/components/auth/AuthButton.test.js
+++ src/components/auth/AuthButton.test.js
@@ -3,33 +3,37 @@ import { MemoryRouter } from "react-router";
 import { createServer } from "../../test/server";
 import AuthButtons from "./AuthButtons";
 
-createServer([
-	{
-		path: "/api/user",
-		res: () => {
-			return { user: null };
+describe("when user is not signed in", () => {
+	createServer([
+		{
+			path: "/api/user",
+			res: () => {
+				return { user: null };
+			},
 		},
-	},
-]);
-test("when user is not signed in, sign in and sign up are visible", async () => {
-	console.log("Test 1");
-});
-test("when user is not signed in, sign is not visible", async () => {
-	console.log("Test 2");
+	]);
+	test("when user is not signed in, sign in and sign up are visible", async () => {
+		console.log("Test 1");
+	});
+	test("when user is not signed in, sign is not visible", async () => {
+		console.log("Test 2");
+	});
 });
 
-createServer([
-	{
-		path: "/api/user",
-		res: () => {
-			return { user: { id: 3, email: "test@example.com" } };
+describe("when user is signed in", () => {
+	createServer([
+		{
+			path: "/api/user",
+			res: () => {
+				return { user: { id: 3, email: "test@example.com" } };
+			},
 		},
-	},
-]);
+	]);
 
-test("when user is signed in, sign in and sign up are not visible", async () => {
-	console.log("Test 3");
-});
-test("when user is signed in, sign out is visible", async () => {
-	console.log("Test 4");
+	test("when user is signed in, sign in and sign up are not visible", async () => {
+		console.log("Test 3");
+	});
+	test("when user is signed in, sign out is visible", async () => {
+		console.log("Test 4");
+	});
 });
