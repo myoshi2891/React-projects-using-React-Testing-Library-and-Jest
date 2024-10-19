b58a02158566159d768f61043d96f33489da667d
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-19 19:35:59 +0900

    Making a Reusable Create Server Function

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,50 +1,26 @@
 import { render, screen } from "@testing-library/react";
-import { setupServer } from "msw/node";
-import { rest } from "msw";
 import { MemoryRouter } from "react-router-dom";
+import { createServer } from "../test/server";
 import HomeRoute from "./HomeRoute";
 
 createServer([
 	{
 		path: "/api/repositories",
 		method: "get",
-		res: (req, res, ctx) => {
+		res: (req) => {
+			const language = req.url.searchParams
+				.get("q")
+				.split("language:")[1];
+
 			return {
-				items: [{}, {}],
-			};
-		},
-	},
-]);
-
-const handlers = [
-	rest.get("/api/repositories", (req, res, ctx) => {
-		const language = req.url.searchParams.get("q").split("language:")[1];
-		console.log(language);
-
-		return res(
-			ctx.json({
 				items: [
 					{ id: 1, full_name: `${language}_one` },
 					{ id: 2, full_name: `${language}_two` },
 				],
-			})
-		);
-	}),
-];
-
-const server = setupServer(...handlers);
-
-beforeAll(() => {
-	server.listen();
-});
-
-afterEach(() => {
-	server.resetHandlers();
-});
-
-afterAll(() => {
-	server.close();
-});
+			};
+		},
+	},
+]);
 
 test("renders two links for each language", async () => {
 	render(
