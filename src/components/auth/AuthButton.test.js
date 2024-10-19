2ec43dadd632393408a48ff6fafe8270b1db55b8
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-19 19:53:54 +0900

    Order of Execution

--- src/components/auth/AuthButton.test.js
+++ src/components/auth/AuthButton.test.js
@@ -3,8 +3,33 @@ import { MemoryRouter } from "react-router";
 import { createServer } from "../../test/server";
 import AuthButtons from "./AuthButtons";
 
-test("when user is not signed in, sign in and sign up are visible", async () => {});
-test("when user is not signed in, sign is not visible", async () => {});
+createServer([
+	{
+		path: "/api/user",
+		res: () => {
+			return { user: null };
+		},
+	},
+]);
+test("when user is not signed in, sign in and sign up are visible", async () => {
+	console.log("Test 1");
+});
+test("when user is not signed in, sign is not visible", async () => {
+	console.log("Test 2");
+});
 
-test("when user is signed in, sign in and sign up are not visible", async () => {});
-test("when user is signed in, sign out is visible", async () => {});
+createServer([
+	{
+		path: "/api/user",
+		res: () => {
+			return { user: { id: 3, email: "test@example.com" } };
+		},
+	},
+]);
+
+test("when user is signed in, sign in and sign up are not visible", async () => {
+	console.log("Test 3");
+});
+test("when user is signed in, sign out is visible", async () => {
+	console.log("Test 4");
+});
